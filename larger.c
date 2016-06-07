0243d26738805dff2858cedb26d1a9807e1051a0
SubashreeB <subabakth07@gmail.com>
2016-06-07 11:14:39 +0530

    Create larger.c

---
+++ larger.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int a,c,b;
+  scanf("%d%d%d",&a,&b,&c);
+  if(a>b)
+  {
+   printf("a is larger than b&c");
+  }
+  else if(b>c)
+  {
+     printf("b is larger than c&a");
+  }
+  else
+  {
+       printf("c is larger than b&a");
+  }
+}
